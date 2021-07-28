14a5bec60de6b11b037578b8b850760096e66a41
Fernando Vinícius Gianini Silva <gianini@angularlabs.com.br>
2021-07-28 11:13:23 -0300

    more structure settings

--- android/app/src/main/java/com/firebaseplayground/MainApplication.java
+++ android/app/src/main/java/com/firebaseplayground/MainApplication.java
@@ -1,5 +1,7 @@
 package com.firebaseplayground;
 
+import com.swmansion.reanimated.ReanimatedPackage;
+
 import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
@@ -25,7 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
-          // packages.add(new MyReactNativePackage());
+          // packages.add(new ReanimatedPackage());
           return packages;
         }
 
---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+
+import Routes from './navigations';
+
+const App = () => (
+  <Routes />
+);
+
+export default App;
