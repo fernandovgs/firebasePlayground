70c8232ee409b42fb93126baff9e0cfc91b8e924
Fernando Vinícius Gianini Silva <gianini@angularlabs.com.br>
2021-09-15 10:58:37 -0300

    extended locale auto setup for android app name

--- setLocale.js
+++ setLocale.js
@@ -11,3 +11,13 @@ fs.readFile(`src/i18n/configLocales/${locale}.js`, 'utf8', (error, data) => {
     if (err) throw new Error(err);
   });
 });
+
+fs.readFile(`android/app/src/main/res/values/strings-${locale}.xml`, 'utf8', (error, data) => {
+  if (error) {
+    throw new Error(error);
+  }
+
+  fs.writeFile('android/app/src/main/res/values/strings.xml', data, 'utf8', (err) => {
+    if (err) throw new Error(err);
+  });
+});
