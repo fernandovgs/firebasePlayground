ecd7f910ba3257f8db5614fffba584d3b6c6f14c
Fernando Vinícius Gianini Silva <gianini@angularlabs.com.br>
2021-09-15 11:43:03 -0300

    automated build for different locales - prototype

--- setLocale.js
+++ setLocale.js
@@ -12,12 +12,22 @@ fs.readFile(`src/i18n/configLocales/${locale}.js`, 'utf8', (error, data) => {
   });
 });
 
-fs.readFile(`android/app/src/main/res/values/strings-${locale}.xml`, 'utf8', (error, data) => {
+fs.readFile(`androidLocales/strings-${locale}.xml`, 'utf8', (error, data) => {
   if (error) {
     throw new Error(error);
   }
 
   fs.writeFile('android/app/src/main/res/values/strings.xml', data, 'utf8', (err) => {
+    if (err) throw new Error(err);
+  });
+});
+
+fs.readFile(`iosLocales/Info-${locale}.plist`, 'utf8', (error, data) => {
+  if (error) {
+    throw new Error(error);
+  }
+
+  fs.writeFile('ios/firebasePlayground/Info.plist', data, 'utf8', (err) => {
     if (err) throw new Error(err);
   });
 });
